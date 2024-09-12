7986083d8f967e7e169661fb39aaff83c380cf49
Olga Lipina <olga@talent-hack.ru-central1.internal>
2024-09-12 10:38:27 +0000

    Fixed no run. Universal build

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 @party_test_1_bot
 
 ## How to run
-`docker compose up -d bot --build`
+1. Скопируйте .env.example, создайте .env файл и заполните его.
+2. Если нужно, переименуйте контейнер в docker-compose.yaml с `aiogram_bot` на то, что вам нужно.
+2.  `docker compose up -d bot --build`
--- src/consts.py
+++ src/consts.py
@@ -3,20 +3,17 @@ import os
 from pathlib import Path
 from dotenv import dotenv_values
 
-
 PROJECT_PATH = Path(__file__).parent.parent
 ENV_PATH = PROJECT_PATH / ".env"
 config_env = dotenv_values(ENV_PATH)
 
 YANDEX_API_KEY = os.environ.get(
-    "YANDEX_API_KEY", default=config_env.get("YANDEX_API_KEY"))
+    "YANDEX_API_KEY", config_env.get("YANDEX_API_KEY"))
 YANDEX_FOLDER_ID = os.environ.get(
-    "YANDEX_FOLDER_ID", default=config_env.get("YANDEX_FOLDER_ID"))
+    "YANDEX_FOLDER_ID", config_env.get("YANDEX_FOLDER_ID"))
 OPENAI_API_KEY = os.environ.get(
-    "OPENAI_API_KEY", default=config_env.get("OPENAI_API_KEY"))
-PROXY_URL = os.environ.get(
-    "PROXY_URL", default=config_env.get("PROXY_URL"))
-TG_TOKEN = os.environ.get(
-    "TG_TOKEN", default=config_env.get("TG_TOKEN"))
+    "OPENAI_API_KEY", config_env.get("OPENAI_API_KEY"))
+PROXY_URL = os.environ.get("PROXY_URL", config_env.get("PROXY_URL"))
+TG_TOKEN = os.environ.get("TG_TOKEN", config_env.get("TG_TOKEN"))
 SQLALCHEMY_URL = os.environ.get(
-    "SQLALCHEMY_URL", default=config_env.get("SQLALCHEMY_URL"))
+    "SQLALCHEMY_URL", config_env.get("SQLALCHEMY_URL"))
